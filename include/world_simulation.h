aa683bb5d2dcead8785200f9b47f6999caa602bf
TimoGuy <timoguy99004@gmail.com>
2025-01-23 08:13:20 -0800

    Seems like this is the right track. It's still WIP tho.

--- include/world_simulation.h
+++ include/world_simulation.h
@@ -45,11 +45,16 @@ private:
     class J2_execute_simulation_tick_job : public Job_ifc
     {
     public:
-        J2_execute_simulation_tick_job(Job_source& source, World_simulation& world_sim, pool_elem_key_t key)
-            : Job_ifc("World Simulation execute sim tick job", source)
+        J2_execute_simulation_tick_job(World_simulation& world_sim)
+            : Job_ifc("World Simulation execute sim tick job", world_sim)
             , m_world_sim(world_sim)
-            , m_elem_key(key)
+            , m_elem_key(0)
         {
+        }
+
+        void set_elem_key(pool_elem_key_t new_key)
+        {
+            m_elem_key = new_key;
         }
 
         int32_t execute() override;
@@ -58,22 +63,24 @@ private:
         World_simulation& m_world_sim;
         pool_elem_key_t m_elem_key;
     };
-    std::vector<std::unique_ptr<J2_execute_simulation_tick_job>> m_j2_execute_simulation_tick_jobs;  // @TODO: START HERE!!!!! AND GET THE SIM TICK JOBS FILLED OUT!!!! MAYBE HAVE LIKE A POOL OF THESE?????
+    std::vector<std::unique_ptr<J2_execute_simulation_tick_job>> m_j2_execute_simulation_tick_jobs;
 
     class J3_remove_pending_objs_job : public Job_ifc
     {
     public:
         J3_remove_pending_objs_job(
-            Job_source& source,
+            World_simulation& world_sim,
             std::vector<pool_elem_key_t>& elem_keys_ref)
-            : Job_ifc("World Simulation remove pending objs job", source)
+            : Job_ifc("World Simulation remove pending objs job", world_sim)
+            , m_world_sim(world_sim)
             , m_elem_keys(elem_keys_ref)
         {
         }
 
         int32_t execute() override;
 
-        std::vector<pool_elem_key_t>& m_elem_keys;
+        World_simulation& m_world_sim;
+        std::vector<pool_elem_key_t>& m_elem_keys;  // @TODO: remove or refactor.
     };
     std::unique_ptr<J3_remove_pending_objs_job> m_j3_remove_pending_objs_job;
 
@@ -81,16 +88,18 @@ private:
     {
     public:
         J4_add_pending_objs_job(
-            Job_source& source,
+            World_simulation& world_sim,
             std::vector<std::unique_ptr<Simulating_entity_ifc>>& elems_ref)
-            : Job_ifc("World Simulation add pending objs job", source)
+            : Job_ifc("World Simulation add pending objs job", world_sim)
+            , m_world_sim(world_sim)
             , m_elems(elems_ref)
         {
         }
 
         int32_t execute() override;
 
-        std::vector<std::unique_ptr<Simulating_entity_ifc>>& m_elems;
+        World_simulation& m_world_sim;
+        std::vector<std::unique_ptr<Simulating_entity_ifc>>& m_elems;  // @TODO: remove or refactor.
     };
     std::unique_ptr<J4_add_pending_objs_job> m_j4_add_pending_objs_job;
 
@@ -128,12 +137,68 @@ private:
         if (idx >= m_data_pool.size())
             return nullptr;
 
-        auto& dwv{ m_data_pool[idx] };
-        if (dwv.version_num != version_num)
+        auto& pool_elem{ m_data_pool[idx] };
+        uint32_t pool_elem_version_num{ pool_elem.version_num.load() };
+        if (pool_elem_version_num == (uint32_t)-1 ||
+            pool_elem_version_num != version_num)
             return nullptr;
 
-        return dwv.data.get();
+        return pool_elem.data.get();
+    }
+
+    pool_elem_key_t get_sim_entity_key_from_index(uint32_t idx)
+    {
+        pool_elem_key_t key{
+            static_cast<uint64_t>(idx) |
+            static_cast<uint64_t>(m_data_pool[idx].version_num.load()) << 32
+        };
+        return key;
+    }
+
+    bool delete_sim_entity(pool_elem_key_t key)
+    {
+        uint32_t idx{ key & 0x00000000ffffffff };
+        uint32_t version_num{ (key & 0xffffffff00000000) >> 32 };
+        
+        if (idx >= m_data_pool.size())
+            return false;
+
+        // Remove elem from data pool and active indices list.
+        auto& pool_elem{ m_data_pool[idx] };
+        pool_elem.data.reset();
+        pool_elem.data = nullptr;  // @CHECK: is this necessary?????
+        m_active_data_pool_indices.erase(
+            std::remove(
+                m_active_data_pool_indices.begin(),
+                m_active_data_pool_indices.end(),
+                idx
+            ),
+            m_active_data_pool_indices.end()
+        );
+        return true;
+    }
+
+    bool add_sim_entity(std::unique_ptr<Simulating_entity_ifc>&& sim_entity_to_insert, uint32_t& pool_idx_ref)
+    {
+        bool found{ false };
+        for (; pool_idx_ref < m_data_pool.size() && !found; pool_idx_ref++)
+        {
+            // Scan data pool for empty position.
+            auto& pool_elem{ m_data_pool[pool_idx_ref] };
+            if (pool_elem.version_num == (uint32_t)-1 ||
+                pool_elem.data == nullptr)
+            {
+                // Add elem into data pool and active indices list.
+                pool_elem.data = std::move(sim_entity_to_insert);
+                pool_elem.version_num++;
+                m_active_data_pool_indices.emplace_back(pool_idx_ref);
+                found = true;
+            }
+        }
+        return found;
     }
 
     std::array<Data_with_version, 4096> m_data_pool;
+    std::vector<uint32_t> m_active_data_pool_indices;
+    std::mutex m_data_pool_mutex;
 };
--- src/world_simulation.cpp
+++ src/world_simulation.cpp
@@ -1,6 +1,7 @@
 #include "world_simulation.h"
 
 #include <chrono>
+#include <iostream>
 #include "simulating_entity_ifc.h"
 
 
@@ -17,7 +18,7 @@ World_simulation::World_simulation()
 void World_simulation::add_sim_entity_to_world(std::unique_ptr<Simulating_entity_ifc>&& entity)
 {
     std::lock_guard<std::mutex> lock{ m_insertion_queue_mutex };
-    m_insertion_queue.emplace_back(entity);
+    m_insertion_queue.emplace_back(std::move(entity));
 }
 
 void World_simulation::remove_entity_from_world(pool_elem_key_t entity_key)
@@ -27,12 +28,6 @@ void World_simulation::remove_entity_from_world(pool_elem_key_t entity_key)
 }
 
 // Jobs.
-int32_t World_simulation::J1_calc_start_next_tick_job::execute()
-{
-    m_world_sim
-    return 0;
-}
-
 int32_t World_simulation::J2_execute_simulation_tick_job::execute()
 {
     m_world_sim.get_sim_entity(m_elem_key)->tick_sim_entity();
@@ -41,11 +36,35 @@ int32_t World_simulation::J2_execute_simulation_tick_job::execute()
 
 int32_t World_simulation::J3_remove_pending_objs_job::execute()
 {
+    std::lock_guard<std::mutex> lock1{ m_world_sim.m_deletion_queue_mutex };
+    std::lock_guard<std::mutex> lock2{ m_world_sim.m_data_pool_mutex };
+    for (auto key : m_world_sim.m_deletion_queue)
+    {
+        bool result{
+            m_world_sim.delete_sim_entity(key)
+        };
+        if (!result)
+            std::cerr << "ERROR: Deleting key " << key << " failed." << std::endl;
+    }
+    m_world_sim.m_deletion_queue.clear();
     return 0;
 }
 
 int32_t World_simulation::J4_add_pending_objs_job::execute()
 {
+    std::lock_guard<std::mutex> lock1{ m_world_sim.m_insertion_queue_mutex };
+    std::lock_guard<std::mutex> lock2{ m_world_sim.m_data_pool_mutex };
+
+    uint32_t pool_idx_iter_pos{ 0 };
+    for (auto& sim_entity_uptr : m_world_sim.m_insertion_queue)
+    {
+        bool result{
+            m_world_sim.add_sim_entity(std::move(sim_entity_uptr), pool_idx_iter_pos)
+        };
+        if (!result)
+            std::cerr << "ERROR: Inserting sim entity failed." << std::endl;
+    }
+    m_world_sim.m_insertion_queue.clear();
     return 0;
 }
 
@@ -65,9 +84,39 @@ std::vector<Job_ifc*> World_simulation::fetch_next_jobs_callback()
             break;
 
         case Job_source_state::EXECUTE_SIMULATION_TICKS:
+        {
+            std::lock_guard<std::mutex> lock{ m_data_pool_mutex };
+            
+            // Expand tick jobs buffer with, don't contract.
+            if (m_active_data_pool_indices.size() > m_j2_execute_simulation_tick_jobs.size())
+            {
+                size_t num_new_jobs{
+                    m_active_data_pool_indices.size() - m_j2_execute_simulation_tick_jobs.size()
+                };
+                m_j2_execute_simulation_tick_jobs.reserve(m_active_data_pool_indices.size());
+                for (size_t _ = 0; _ < num_new_jobs; _++)
+                {
+                    m_j2_execute_simulation_tick_jobs.emplace_back(
+                        std::make_unique<J2_execute_simulation_tick_job>(*this)
+                    );
+                }
+            }
+
+            // Fill in tick job indices and add to jobs.
+            size_t num_jobs{ m_active_data_pool_indices.size() };
+            jobs.reserve(num_jobs);
+            for (size_t i = 0; i < num_jobs; i++)
+            {
+                auto pool_idx{ m_active_data_pool_indices[i] };
+                m_j2_execute_simulation_tick_jobs[i]->set_elem_key(
+                    get_sim_entity_key_from_index(pool_idx)
+                );
+                jobs.emplace_back(m_j2_execute_simulation_tick_jobs[i].get());
+            }
 
             m_current_state = Job_source_state::REMOVE_PENDING_OBJS;
-            break;
+        }
+        break;
 
         case Job_source_state::REMOVE_PENDING_OBJS:
             jobs.emplace_back(m_j3_remove_pending_objs_job.get());
