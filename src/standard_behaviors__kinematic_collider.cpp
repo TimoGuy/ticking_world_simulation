d6df9f5fb14b47dd7f165b0e5decc0c68d40d96d
TimoGuy <timoguy99004@gmail.com>
2025-04-01 04:07:56 -0700

    WIP: Saving addition to update function.

--- src/standard_behaviors__kinematic_collider.cpp
+++ src/standard_behaviors__kinematic_collider.cpp
@@ -15,6 +15,33 @@ void std_behavior::Kinematic_collider::on_update()
     auto& input_data{
         get_data_from_input<Kinematic_collider_transform_input_data>() };
 
-    // @TODO: STUB.
-    assert(false);
+    switch (input_data.type)
+    {
+    case TRANS_DATA_TYPE_NONE:
+        // Do nothing.
+        break;
+
+    case TRANS_DATA_TYPE_SET:
+        m_phys_kinematic_actor.set_transform(input_data.position,
+                                             input_data.rotation);
+        break;
+
+    case TRANS_DATA_TYPE_MOVE_ABSOLUTE:
+        m_phys_kinematic_actor.move_kinematic(input_data.position,
+                                              input_data.rotation);
+        break;
+
+    case TRANS_DATA_TYPE_MOVE_DELTA:
+    {
+        JPH::RVec3 prev_position{ m_phys_kinematic_actor.get_position() };
+        JPH::Quat  prev_rotation{ m_phys_kinematic_actor.get_rotation() };
+        m_phys_kinematic_actor.move_kinematic(prev_position + input_data.position,
+                                              prev_rotation * input_data.rotation);
+        break;
+    }
+
+    default:
+        assert(false);
+        break;
+    }
 }
